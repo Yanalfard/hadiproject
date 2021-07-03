ad2c1b214507c2bf70e376fe0e76ad78d46bc60a
nazila bagheri <nazila.bagheri93@gmail.com>
2021-07-03 10:20:33 +0430

    12tir

---
+++ Scripts/script.js
@@ -0,0 +1,94 @@
+function step1() {
+    document.getElementById("step1Dot").style.borderColor = "black";
+    document.getElementById("step1InnerDot").style.background = "red";
+    document.getElementById("step1Date").style.color = "black";
+    document.getElementById("step1Line").style.borderTopColor = "black";
+    document.getElementById("step1Chev").classList.remove("d-none");
+    document.getElementById("step2Dot").style.borderColor = "black";
+    document.getElementById("step2InnerDot").style.background = "red";
+
+
+    document.getElementById("step2Date").style.color = "#B2B2B2";
+    document.getElementById("step2Line").style.borderTopColor = "#B2B2B2";
+    document.getElementById("step2Chev").classList.add("d-none");
+    document.getElementById("step3Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step3InnerDot").style.background = "#B2B2B2";
+
+    document.getElementById("step3Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step3InnerDot").style.background = "#B2B2B2";
+    document.getElementById("step3Date").style.color = "#B2B2B2";
+    document.getElementById("step3Line").style.borderTopColor = "#B2B2B2";
+    document.getElementById("step3Chev").classList.add("d-none");
+    document.getElementById("step4Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step4InnerDot").style.background = "#B2B2B2";
+
+    document.getElementById("Game-Submission").style.borderColor = "black";
+
+}
+
+function step2() {
+    document.getElementById("step2Dot").style.borderColor = "black";
+    document.getElementById("step2InnerDot").style.background = "red";
+    document.getElementById("step2Date").style.color = "black";
+    document.getElementById("step2Line").style.borderTopColor = "black";
+    document.getElementById("step2Chev").classList.remove("d-none");
+    document.getElementById("step3Dot").style.borderColor = "black";
+    document.getElementById("step3InnerDot").style.background = "red";
+
+    document.getElementById("step3Date").style.color = "#B2B2B2";
+    document.getElementById("step3Line").style.borderTopColor = "#B2B2B2";
+    document.getElementById("step3Chev").classList.add("d-none");
+    document.getElementById("step4Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step4InnerDot").style.background = "#B2B2B2";
+
+    document.getElementById("step1Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step1InnerDot").style.background = "#B2B2B2";
+    document.getElementById("step1Date").style.color = "#B2B2B2";
+    document.getElementById("step1Line").style.borderTopColor = "#B2B2B2";
+    document.getElementById("step1Chev").classList.add("d-none");
+
+    document.getElementById("Shortlisting").style.borderColor = "black";
+
+}
+
+function step3() {
+    document.getElementById("step3Dot").style.borderColor = "black";
+    document.getElementById("step3InnerDot").style.background = "red";
+    document.getElementById("step3Date").style.color = "black";
+    document.getElementById("step3Line").style.borderTopColor = "black";
+    document.getElementById("step3Chev").classList.remove("d-none");
+    document.getElementById("step4Dot").style.borderColor = "black";
+    document.getElementById("step4InnerDot").style.background = "red";
+
+
+    document.getElementById("step2Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step2InnerDot").style.background = "#B2B2B2";
+    document.getElementById("step2Date").style.color = "#B2B2B2";
+    document.getElementById("step2Line").style.borderTopColor = "#B2B2B2";
+    document.getElementById("step2Chev").classList.add("d-none");
+
+    document.getElementById("step1Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step1InnerDot").style.background = "#B2B2B2";
+    document.getElementById("step1Date").style.color = "#B2B2B2";
+    document.getElementById("step1Line").style.borderTopColor = "#B2B2B2";
+    document.getElementById("step1Chev").classList.add("d-none");
+    document.getElementById("step2Dot").style.borderColor = "#B2B2B2";
+    document.getElementById("step2InnerDot").style.background = "#B2B2B2";
+
+    document.getElementById("Final-Round").style.borderColor = "black";
+
+}
+
+
+// document.getElementById("header-page").addEventListener("scroll", animation());
+var flag = false
+if (flag == false) {
+    window.onscroll = function() { animation() };
+    flag = true
+}
+
+function animation() {
+    document.getElementById("left-airplane").classList.add("animation");
+    document.getElementById("right-airplane").classList.add("animation1");
+
+}
