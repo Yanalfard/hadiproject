e939a0b7744b9cf53aba692da5079384bdb76bf6
Yanal Ahmadzade <yanal.fard@gmail.com>
2021-07-03 12:33:13 +0430

    hero animation

--- Scripts/script.js
+++ Scripts/script.js
@@ -81,14 +81,3 @@ function step3() {
 
 
 // document.getElementById("header-page").addEventListener("scroll", animation());
-var flag = false
-if (flag == false) {
-    window.onscroll = function() { animation() };
-    flag = true
-}
-
-function animation() {
-    document.getElementById("left-airplane").classList.add("animation");
-    document.getElementById("right-airplane").classList.add("animation1");
-
-}
